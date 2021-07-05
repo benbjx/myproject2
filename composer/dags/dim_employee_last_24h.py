084e770014d359c3dfcf3baa7a2e84bdd48751db
vboi <vincent.boidin@soprasteria.com>
2021-07-05 10:19:03 +0200

    feat test

--- composer/dags/dim_employee_last_24h.py
+++ composer/dags/dim_employee_last_24h.py
@@ -8,7 +8,7 @@ default_args = {
 	'retries': 0,
 	'retry_delay': timedelta(minutes=5)
 }
-#comment
+#comment 2
 mon_env = models.Variable.get('gcp_project')
 mon_pattern_date = get_pattern_date("LAST_24H")
 cron_schedule = None
